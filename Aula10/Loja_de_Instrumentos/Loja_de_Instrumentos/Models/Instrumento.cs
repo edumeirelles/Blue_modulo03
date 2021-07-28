fd74b5f7eaa3bc48b474e419a1d1da2819e05f8c
Eduardo <edumeirelles@yahoo.com.br>
2021-07-28 19:38:52 -0300

    aula10 - projeto final

---
+++ Aula10/Loja_de_Instrumentos/Loja_de_Instrumentos/Models/Instrumento.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Loja_de_Instrumentos.Models
+{
+    public class Instrumento
+    {
+        [Display(Name = "#")]
+        public int Id { get; set; }
+        [Display(Name = "Tipo")]
+        public string Type { get; set; }
+        [Display(Name = "Marca")]
+        public string Brand { get; set; }
+        [Display(Name = "Modelo")]
+        public string Model { get; set; }
+        [Display(Name = "Preço")]
+        public double Price { get; set; }
+        [Display(Name = "Descrição")]
+        public string Description { get; set; }
+        [Display(Name = "Link da imagem")]
+        public string Link { get; set; }
+    }
+}
