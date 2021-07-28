b078ec210cf4efd5ab1ea83be1bdb7ced43e15b5
Eduardo <edumeirelles@yahoo.com.br>
2021-07-28 19:38:16 -0300

    aula08

--- Aula08/Consultorio/Consultorio/Controllers/PacienteController.cs
+++ Aula08/Consultorio/Consultorio/Controllers/PacienteController.cs
@@ -2,14 +2,12 @@
 using Microsoft.AspNetCore.Mvc;
 using System;
 using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
 
 namespace Consultorio.Controllers
 {
     public class PacienteController : Controller
     {
-        public IActionResult Index(int id)
+        public IActionResult Index(int? id)
         {
             //List<Paciente> pacientesTodos = getPaciente();
             //List<Paciente> pacienteFiltrados = new();
@@ -23,19 +21,36 @@ namespace Consultorio.Controllers
             //}
             //ViewBag.listaPacientes = id == 0 ? pacientesTodos : pacienteFiltrados;
             //List<Paciente> pacientesTodos = getPaciente();
-            ViewBag.listaPacientes = id == 0 ? getPaciente() : getPaciente().FindAll(p => p.Id == id);
-            return View();
+            
+            //ViewBag.listaPacientes = id == null ? getPaciente() : getPaciente().FindAll(p => p.Id == id);
+
+
+            List<Paciente> pacientes = id == null ? getPaciente() : getPaciente().FindAll(p => p.Id == id);
+            return View(pacientes);
             
         }
-
-       
+        [HttpGet]
+        public IActionResult Create()
+        {
+            return View();
+        }
+        [HttpPost]
+        public IActionResult Create(Paciente paciente)
+        {
+            
+            List<Paciente> pacientes = getPaciente();
+            paciente.Id = pacientes.Count + 1;
+            pacientes.Add(paciente);
+            
+            return View("Index",pacientes);
+        }
 
         List<Paciente> getPaciente()
         {
             List<Paciente> listaPacientes = new();
-            listaPacientes.Add(new Paciente { Id = 1, Name = "Eduardo" });
-            listaPacientes.Add(new Paciente { Id = 2, Name = "Zé" });
-            listaPacientes.Add(new Paciente { Id = 3, Name = "Jão" });
+            listaPacientes.Add(new Paciente { Id = 1, Name = "Eduardo" , DataNasc = new DateTime(1985, 01, 15), Descricao = "Aluno" });
+            listaPacientes.Add(new Paciente { Id = 2, Name = "Zé" , DataNasc = new DateTime(1990, 01, 03) , Descricao = "Aluno"});
+            listaPacientes.Add(new Paciente { Id = 3, Name = "Jão" , DataNasc = new DateTime(1976, 04, 26), Descricao = "Professor"});
             return listaPacientes;
         }
     }
--- Aula08/Consultorio/Consultorio/Models/Paciente.cs
+++ Aula08/Consultorio/Consultorio/Models/Paciente.cs
@@ -1,14 +1,21 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
+using System.ComponentModel.DataAnnotations;
 
 namespace Consultorio.Models
 {
     public class Paciente
     {
+        [Display (Name = "#")]
         public int Id { get; set; }
+
+        [Display (Name = "Nome")]
         public string Name { get; set; }
+
+        [Display (Name = "Data de Nascimento")]
+        [DataType(DataType.Date)]
         public DateTime DataNasc { get; set; }
+
+        [Display (Name = "Descrição")]
+        public string Descricao { get; set; }
     }
 }
